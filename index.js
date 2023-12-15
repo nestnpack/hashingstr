7940b2112c22d916697114e6bff49c56b2e8cf6b
haidarahmadfaiq <147744876+haidarahmadfaiq@users.noreply.github.com>
2023-12-16 01:44:10 +0700

    add file index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+/*
+MIT License
+Copyright (c) 2023 Nest Packages
+
+ ---------- Hashstr ---------- 
+*/
+
+const hash = require('./lib/hash.js');
+const compare = require('./lib/compare.js');
+
+module.exports = {
+  hash,
+  compare
+}
